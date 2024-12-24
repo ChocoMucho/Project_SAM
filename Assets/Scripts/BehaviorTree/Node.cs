88f77ed832dac5da0781c609c198dcf4f1bc90df
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2024-12-24 16:01:06 +0900

    Feat: BehaviorTree - Node

---
+++ Assets/Scripts/BehaviorTree/Node.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public Status status;
+    public readonly string name;
+    public readonly List<Node> Children;
+    protected int currentChild; // ¿Œµ¶Ω∫
+
+    public Node(string name)
+    {
+        this.name = name;
+        Children = new List<Node>();
+        currentChild = 0;
+    }
+
+    public void AddChild(Node child) => Children.Add(child);
+
+    public virtual Status Process() => Children[currentChild].Process();
+
+    public virtual void Reset()
+    {
+        currentChild = 0;
+        foreach (Node child in Children)
+        {
+            child.Reset();
+        }
+    }
+    
+}
